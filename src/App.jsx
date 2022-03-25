d4e1ab621bca8b2d8e3db7a76314b6399bb56fc7
MehdiRazaNaqvi <mehdiraza1232016@gmail.com>
2022-03-25 17:24:08 +0500

    dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,25 @@
 
+import { useState } from 'react';
 import { connect } from 'react-redux';
 import './App.css';
 
 
 
-import {getitems} from "./store/action/action"
+
+import { getitems } from "./store/action/action"
 
 function App(props) {
+
+  const [lightmode, setmode] = useState(true)
 
 
 
   return (
 
-    <div className="parent">
+    <div className={lightmode? "parent" : "parent dark"}>
 
-
-      <div className="left"></div>
+ 
+      <div className="left" > </div>
 
 
 
@@ -23,6 +27,20 @@ function App(props) {
 
         <div className="search">
 
+          <form>
+            <div className="form-row align-items-center">
+              <div className="col-auto my-1">
+
+                <select onChange={() => { setmode(!lightmode); console.log(lightmode) }} className="custom-select mr-sm-2" id="inlineFormCustomSelect">
+
+                  <option defaultChecked value="1">Light</option>
+                  <option value="2">Dark</option>
+
+                </select>
+              </div>
+
+            </div>
+          </form>
 
         </div>
 
@@ -33,9 +51,9 @@ function App(props) {
           <span>
 
 
-            <div class="card bg-dark text-white">
+            <div className="card">
 
-              <img src={props.img1} class="card-img" />
+              <img src={props.img1} className="card-img" />
               <div className="card-img-overlay">
                 <h6 className="card-title">{props.name1}</h6>
                 <h6 className='price'>Rs. {props.price1}</h6>
@@ -48,10 +66,10 @@ function App(props) {
 
           <span>
 
-            <div class="card bg-dark text-white">
-              <img src={props.img2} class="card-img" />
-              <div class="card-img-overlay">
-              <h6 className="card-title">{props.name2}</h6>
+            <div className="card">
+              <img src={props.img2} className="card-img" />
+              <div className="card-img-overlay">
+                <h6 className="card-title">{props.name2}</h6>
                 <h6 className='price'>Rs. {props.price2}</h6>
               </div>
             </div>
@@ -61,10 +79,10 @@ function App(props) {
 
           <span>
 
-            <div class="card bg-dark text-white">
-              <img src={props.img3} class="card-img" />
-              <div class="card-img-overlay">
-              <h6 className="card-title">{props.name3}</h6>
+            <div className="card">
+              <img src={props.img3} className="card-img" />
+              <div className="card-img-overlay">
+                <h6 className="card-title">{props.name3}</h6>
                 <h6 className='price'>Rs. {props.price3}</h6>
               </div>
             </div>
@@ -73,28 +91,28 @@ function App(props) {
 
           <span>
 
-          <div class="card bg-dark text-white">
-              <img src={props.img4} class="card-img" />
-              <div class="card-img-overlay">
-              <h6 className="card-title">{props.name4}</h6>
+            <div className="card">
+              <img src={props.img4} className="card-img" />
+              <div className="card-img-overlay">
+                <h6 className="card-title">{props.name4}</h6>
                 <h6 className='price'>Rs. {props.price4}</h6>
               </div>
             </div>
 
           </span>
-          
+
           <span>
 
-          <div class="card bg-dark text-white">
-              <img src={props.img5} class="card-img" />
-              <div class="card-img-overlay">
-              <h6 className="card-title">{props.name5}</h6>
+            <div className="card">
+              <img src={props.img5} className="card-img" />
+              <div className="card-img-overlay">
+                <h6 className="card-title">{props.name5}</h6>
                 <h6 className='price'>Rs. {props.price5}</h6>
               </div>
             </div>
 
           </span>
-          
+
           <span></span><span></span><span></span><span></span><span></span><span></span><span></span><span></span><span></span><span></span>
         </div>
 
@@ -105,11 +123,11 @@ function App(props) {
       <div class="right">
         <button onClick={() => {
 
-          
+
           props.getitems()
 
         }}  >Get Items</button>
-      </div>
+      </div> 
 
     </div>
 
@@ -120,32 +138,32 @@ function App(props) {
 const mapStateToProps = (state) => ({
   name1: state.name1,
   price1: state.price1,
-  img1 : state.img1,
+  img1: state.img1,
 
-  name2 : state.name2,
-  price2 : state.price2,
-  img2 : state.img2,
+  name2: state.name2,
+  price2: state.price2,
+  img2: state.img2,
 
-  name3 : state.name3,
-  price3 : state.price3,
-  img3 : state.img3,
+  name3: state.name3,
+  price3: state.price3,
+  img3: state.img3,
 
-  name4 : state.name4,
-  price4 : state.price4,
-  img4 : state.img4,
+  name4: state.name4,
+  price4: state.price4,
+  img4: state.img4,
 
-  name5 : state.name5,
-  price5 : state.price5,
-  img5 : state.img5,
+  name5: state.name5,
+  price5: state.price5,
+  img5: state.img5,
 })
 
 
 const mapDispatchToProps = (dispatch) => ({
-  getitems : () => dispatch(getitems())
+  getitems: () => dispatch(getitems())
 })
 
 
-export default connect( mapStateToProps, mapDispatchToProps )(App)
+export default connect(mapStateToProps, mapDispatchToProps)(App)
 
 
 
--- src/store/reducer/index.jsx
+++ src/store/reducer/index.jsx
@@ -11,17 +11,17 @@ export default (state = ini, action) => {
         case ("item2"):
             return { ...state, name2: action.payload.name, price2: action.payload.price, img2: action.payload.img }
 
-            
+
 
         case ("item3"):
             return { ...state, name3: action.payload.name, price3: action.payload.price, img3: action.payload.img }
 
-            case ("item4"):
-                return { ...state, name4: action.payload.name , price4: action.payload.price , img4: action.payload.img }
+        case ("item4"):
+            return { ...state, name4: action.payload.name, price4: action.payload.price, img4: action.payload.img }
 
-                
-            case ("item5"):
-                return { ...state, name5: action.payload.name , price5: action.payload.price , img5: action.payload.img }
+
+        case ("item5"):
+            return { ...state, name5: action.payload.name, price5: action.payload.price, img5: action.payload.img }
 
         default:
             return state
