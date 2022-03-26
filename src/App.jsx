4161e49113bad2bfa9442ddee5a2802ba33f1cb8
MehdiRazaNaqvi <mehdiraza1232016@gmail.com>
2022-03-26 23:22:25 +0500

    items filled

--- src/App.jsx
+++ src/App.jsx
@@ -16,32 +16,36 @@ function App(props) {
 
   return (
 
-    <div className={lightmode? "parent" : "parent dark"}>
+    <div className={lightmode ? "parent" : "parent dark"}>
 
- 
-      <div className="left" > </div>
+
+      <div className="left" >
+
+
+
+        <form>
+          <div className="form-row align-items-center">
+            <div className="col-auto my-1">
+
+              <select onChange={() => setmode(!lightmode) } className={lightmode ? "btn btn-outline-dark custom-select mr-sm-2" : "btn btn-outline-light custom-select mr-sm-2"} id="inlineFormCustomSelect">
+
+                <option defaultChecked value="1">Light</option>
+                <option value="2">Dark</option>
+
+              </select>
+            </div>
+
+          </div>
+        </form>
+
+      </div>
+
 
 
 
       <div className="center">
 
         <div className="search">
-
-          <form>
-            <div className="form-row align-items-center">
-              <div className="col-auto my-1">
-
-                <select onChange={() => { setmode(!lightmode); console.log(lightmode) }} className="custom-select mr-sm-2" id="inlineFormCustomSelect">
-
-                  <option defaultChecked value="1">Light</option>
-                  <option value="2">Dark</option>
-
-                </select>
-              </div>
-
-            </div>
-          </form>
-
         </div>
 
 
@@ -113,7 +117,126 @@ function App(props) {
 
           </span>
 
-          <span></span><span></span><span></span><span></span><span></span><span></span><span></span><span></span><span></span><span></span>
+          <span>
+            <div className="card">
+              <img src={props.img6} className="card-img" />
+              <div className="card-img-overlay">
+                <h6 className="card-title">{props.name6}</h6>
+                <h6 className='price'>Rs. {props.price6}</h6>
+              </div>
+            </div>
+
+          </span>
+
+
+          <span>
+
+          <div className="card">
+              <img src={props.img7} className="card-img" />
+              <div className="card-img-overlay">
+                <h6 className="card-title">{props.name7}</h6>
+                <h6 className='price'>Rs. {props.price7}</h6>
+              </div>
+            </div>
+          </span>
+          
+          
+          <span>
+
+          <div className="card">
+              <img src={props.img8} className="card-img" />
+              <div className="card-img-overlay">
+                <h6 className="card-title">{props.name8}</h6>
+                <h6 className='price'>Rs. {props.price8}</h6>
+              </div>
+            </div>
+
+          </span>
+          
+          <span>
+
+          <div className="card">
+              <img src={props.img9} className="card-img" />
+              <div className="card-img-overlay">
+                <h6 className="card-title">{props.name9}</h6>
+                <h6 className='price'>Rs. {props.price9}</h6>
+              </div>
+            </div>
+          </span>
+
+          
+          <span>
+
+          <div className="card">
+              <img src={props.img10} className="card-img" />
+              <div className="card-img-overlay">
+                <h6 className="card-title">{props.name10}</h6>
+                <h6 className='price'>Rs. {props.price10}</h6>
+              </div>
+            </div>
+          </span>
+          
+          
+          <span>
+          <div className="card">
+              <img src={props.img11} className="card-img" />
+              <div className="card-img-overlay">
+                <h6 className="card-title">{props.name11}</h6>
+                <h6 className='price'>Rs. {props.price11}</h6>
+              </div>
+            </div>
+
+          </span>
+          
+          
+          <span>
+          <div className="card">
+              <img src={props.img12} className="card-img" />
+              <div className="card-img-overlay">
+                <h6 className="card-title">{props.name12}</h6>
+                <h6 className='price'>Rs. {props.price12}</h6>
+              </div>
+            </div>
+
+          </span>
+          
+          
+          <span>
+
+          <div className="card">
+              <img src={props.img13} className="card-img" />
+              <div className="card-img-overlay">
+                <h6 className="card-title">{props.name13}</h6>
+                <h6 className='price'>Rs. {props.price13}</h6>
+              </div>
+            </div>
+          </span>
+          
+
+          <span>
+            
+          <div className="card">
+              <img src={props.img14} className="card-img" />
+              <div className="card-img-overlay">
+                <h6 className="card-title">{props.name14}</h6>
+                <h6 className='price'>Rs. {props.price14}</h6>
+              </div>
+            </div>
+          </span>
+          
+          
+          <span>
+
+          <div className="card">
+              <img src={props.img15} className="card-img" />
+              <div className="card-img-overlay">
+                <h6 className="card-title">{props.name15}</h6>
+                <h6 className='price'>Rs. {props.price15}</h6>
+              </div>
+            </div>
+          </span>
+
+
         </div>
 
 
@@ -121,13 +244,13 @@ function App(props) {
 
 
       <div class="right">
-        <button onClick={() => {
+        <button className='btn  btn-outline-dark' onClick={() => {
 
 
           props.getitems()
 
-        }}  >Get Items</button>
-      </div> 
+        }}  >Fetch Items</button>
+      </div>
 
     </div>
 
@@ -155,6 +278,46 @@ const mapStateToProps = (state) => ({
   name5: state.name5,
   price5: state.price5,
   img5: state.img5,
+
+  name6: state.name6,
+  price6: state.price6,
+  img6: state.img6,
+
+  name7: state.name7,
+  price7: state.price7,
+  img7: state.img7,
+
+  name8: state.name8,
+  price8: state.price8,
+  img8: state.img8,
+
+  name9: state.name9,
+  price9: state.price9,
+  img9: state.img9,
+
+  name10: state.name10,
+  price10: state.price10,
+  img10: state.img10,
+
+  name11: state.name11,
+  price11: state.price11,
+  img11: state.img11,
+
+  name12: state.name12,
+  price12: state.price12,
+  img12: state.img12,
+
+  name13: state.name13,
+  price13: state.price13,
+  img13: state.img13,
+
+  name14: state.name14,
+  price14: state.price14,
+  img14: state.img14,
+
+  name15: state.name15,
+  price15: state.price15,
+  img15: state.img15,
 })
 
 
--- src/store/action/action.jsx
+++ src/store/action/action.jsx
@@ -14,7 +14,7 @@ const getitems = () => {
         const starCountRef = ref(database, 'items/');
         onValue(starCountRef, (snapshot) => {
             const data = snapshot.val();
-            console.log(data)
+            // console.log(data)
 
             {
 
@@ -23,16 +23,16 @@ const getitems = () => {
             dispatch( { type : "item3" , payload: data.item3} )
             dispatch( { type : "item4" , payload: data.item4} )
             dispatch( { type : "item5" , payload: data.item5} )
-            // dispatch( { type : "item6" , payload: data.item6} )
-            // dispatch( { type : "item7" , payload: data.item7} )
-            // dispatch( { type : "item8" , payload: data.item8} )
-            // dispatch( { type : "item9" , payload: data.item9} )
-            // dispatch( { type : "item10" , payload: data.item10} )
-            // dispatch( { type : "item11" , payload: data.item11} )
-            // dispatch( { type : "item12" , payload: data.item12} )
-            // dispatch( { type : "item13" , payload: data.item13} )
-            // dispatch( { type : "item14" , payload: data.item14} )
-            // dispatch( { type : "item15" , payload: data.item15} )
+            dispatch( { type : "item6" , payload: data.item6} )
+            dispatch( { type : "item7" , payload: data.item7} )
+            dispatch( { type : "item8" , payload: data.item8} )
+            dispatch( { type : "item9" , payload: data.item9} )
+            dispatch( { type : "item10" , payload: data.item10} )
+            dispatch( { type : "item11" , payload: data.item11} )
+            dispatch( { type : "item12" , payload: data.item12} )
+            dispatch( { type : "item13" , payload: data.item13} )
+            dispatch( { type : "item14" , payload: data.item14} )
+            dispatch( { type : "item15" , payload: data.item15} )
             }
 
         });
