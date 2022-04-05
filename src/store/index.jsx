55bbab53b53399667322b6f2a4421b146f35fa9c
MehdiRazaNaqvi <mehdiraza1232016@gmail.com>
2022-04-05 19:57:54 +0500

    Updates

--- src/store/index.jsx
+++
@@ -1,9 +0,0 @@
-import { applyMiddleware, createStore } from "redux"
-import thunk from "redux-thunk";
-import reducer from "./reducer/index"
-
-
-
-const store = createStore(reducer  , applyMiddleware(thunk))
-
-export default store;
